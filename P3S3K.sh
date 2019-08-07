6d018ba7aea7128d9834ddeb3ae683dfe0afc35b
MR-P3S3K <52663525+MR-P3S3K@users.noreply.github.com>
2019-08-07 21:58:46 +0700

    Add files via upload

---
+++ P3S3K.sh
@@ -0,0 +1,293 @@
+#usr/!/sh
+	#JANGANDIRECODE
+
+Cy='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+e='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+clear
+sleep 2
+echo $e"
+■         ■             ■■■■■■   ■■■■■    ■■■■■■■■   ■■■■■■   ■   ■
+■ ■     ■ ■             ■     ■       ■   ■      ■         ■  ■  ■
+■  ■   ■  ■             ■     ■     ■■■     ■            ■■■  ■■■
+■    ■    ■  ■■■■       ■■■■■       ■■■       ■          ■■■  ■■
+■         ■  ■   ■      ■             ■   ■     ■          ■  ■  ■
+■         ■  ■          ■        ■■■■■    ■■■■■■■■   ■■■■■■   ■   ■"
+sleep 3
+echo $Cy"______________________________________________"
+echo $pur"|NO.|"$i"                MENU                 "
+echo $pur"|01.|"$i"       Darks-FB (Massal/Target)      "
+echo $pur"|02.|"$i"       Bruteforce N3W                "
+echo $pur"|03.|"$i"       Yahoo CLONING FB              "
+echo $pur"|04.|"$i"       BRUTEFORCE PHP                "
+echo $pur"|05.|"$i"       HACK FB TOKENS                "
+echo $pur"|06.|"$i"       HACK FB MBF                   "
+echo $pur"|07.|"$i"       PHISING FB (WEEMAN)           "
+echo $pur"|08.|"$i"       TROJAN VIRUS         "
+echo $pur"|09.|"$i"       DDOS IP ORANG                 "
+echo $pur"|10.|"$i"       SPAM SMS                      "
+echo $pur"|11.|"$i"       HACK IG "
+echo $pur"|12.|"$i"       TEMBAK PAKET XL  "
+echo $pur"|13.|"$i"       TES KECPETAN INTERNET "
+echo $pur"|14.|"$i"       LACAK IP ORANG                "
+echo $pur"|15.|"$i"       MELIHAT IKON DAN INFORMASI ANDROID"
+echo $pur"|16.|"$i"       MEMPERCANTIK TERMUX "
+echo $pur"|17.|"$i"       BERMAIN MOON BUGGY"
+echo $pur"|18.|"$i"       CRACK PASSWORD HASH"
+echo $pur"|19.|"$i"       INTSTALL HACKG"
+echo $pur"|20.|"$i"       FAKE CALL"
+echo $pur"|00.|"$i"       EXIT / KELUAR PROGRAM         "
+echo $Cy"=============================================="
+echo "\033[36;1mPILIH NOMORNYA BEB😘😙"
+read -p"==> " p
+
+if [ $p = 01 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone https://github.com/R133F/Darks-fb
+cd Darks-fb
+python2 R13F.py
+fi
+
+if [ $p = 02 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone https://github.com/R133F/BRUTERS
+cd BRUTERS
+python2 fb.py
+fi
+
+if [ $pil = 03 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+fi
+
+if [ $p = 04 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone https://github.com/FR13ND8/fbbrute
+cd fbbrute
+php fb.php
+fi
+
+if [ $p = 05 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone https://github.com/CiKu370/OSIF
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $p = 06 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $p = 07 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone https://github.com/samyoyo/weeman
+cd weeman
+python2 weeman.py
+fi
+
+if [ $p = 08 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+pkg install python2
+pkg install php
+pkg install git
+git clone https://github.com/R133F/Trojans
+cd Trojans
+python2 trojans.py
+fi
+
+if [ $p = 09 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone https://github.com/banghyuu/ddosWD
+cd ddosWD
+sh Ddos.sh
+fi
+
+if [ $p = 10 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone https://github.com/Senitopeng/SpamSms.git
+cd SpamSms
+fi
+
+if [ $p = 11 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone https://github.com/avramit/instahack.git
+cd instalhack
+cat pass.txt
+python2 hackista.py
+fi
+
+if [ $p = 12 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone https://github.com/albertoanggi/xl-py
+pip install -r requirements.txt
+chmod +x app.py 
+python2 app.py
+fi
+
+if [ $p = 13 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+python2 speed.py
+fi
+
+if [ $p = 14 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone https://github.com/maldevel/IPGeolocation
+cd IPGeolocation
+chmod +x ipgeolocation.py
+pip install -r requirements.txt
+python ipgeolocation.py -m
+python ipheolocation.py -t
+fi
+
+if [ $p = 15 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+neofetch
+fi
+
+if [ $p = 16 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+gem install lolcat 
+cd ../usr/etc
+nano bash.bashrc
+cowsay -f eyes Cyber | lolcat
+toilet -f standard Indonesia -F EROR404
+neofetch
+date | lolcat
+fi
+
+if  [ $p = 17 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+pkg install moon-buggy
+moon-buggy
+fi
+
+if [ $p = 18 ]
+then
+clear
+figlet Mr.P3S3K
+sleep 2
+git clone https://github.com/FajriHidayat088/FHX-Hash-Killer/
+cd FHX-Hash-Killer
+python2 FHXHashKiller.py
+fi
+
+if [ $p = 19 ]
+then
+clear
+figlet Mr .P3S3K
+sleep 2
+pkg install python
+pkg install python2
+pkg install git
+pkg install ruby
+pkg install mtools
+pkg install gem
+pkg install toilet
+pkg install figlet
+pip2 install mechanize
+pip install requests
+pip2 install lolcat
+pkg install openssh
+pip2 install bs4
+pip2 install urllib3 chardet certifi idna requests
+pip2 install -r requirements.txt
+git clone https://github.com/Hack-G/HackG
+cd HackG
+sh Hackg.sh
+fi
+
+if [ $p = 20 ]
+then
+clear
+figlet MR.P3S3K
+sleep 2
+pkg install toilet
+pkg install openssh
+pkg install php
+pkg install nodejs
+pkg install nodejs-lts
+pkg install python python2
+pkg install python2-dev
+pkg install git
+pkg install curl
+pkg install figlet
+pkg install ruby
+gem install lolcat
+git clone https://github.com/T14RB373T/FAKECALL
+cd FAKECALL
+bash call.sh
+1
+fi
+
+if [ $p = 00 ]
+then
+clear
+figlet SEE YOU BEB
+fi
+exit
